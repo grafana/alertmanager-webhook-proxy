54f7e73188e86bcd50ed0a48e8f9c38d726aca4f
Alex Martin <alex@suitupalex.com>
2022-04-28 01:48:16 -0500

    pkg: proxy: Test proxied target URL.

--- pkg/proxy/proxy_test.go
+++ pkg/proxy/proxy_test.go
@@ -2,6 +2,8 @@ package proxy
 
 import (
 	"testing"
+
+	"net/http"
 )
 
 type test struct {
@@ -16,10 +18,21 @@ var tests = []test{
 
 func TestNew(t *testing.T) {
 	for _, test := range tests {
-		_, err := New(test.target)
+		p, err := New(test.target)
 
 		if err != nil {
 			t.Errorf("Error: %v", err)
+		}
+
+		req, _ := http.NewRequest("POST", "https://replace.me", nil)
+
+		p.Director(req)
+
+		got := req.URL.Scheme + "://" + req.URL.Host + req.URL.Path
+		want := test.target + "/"
+
+		if got != want {
+			t.Errorf("got %q, wanted %q", got, want)
 		}
 	}
 }
