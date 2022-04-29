ae7c3e905c3deea36d9f39538735b441e2a4c3b4
Alex Martin <alex@suitupalex.com>
2022-04-29 00:53:47 -0500

    cmd: main: Add cmd line flags for template path, target URL, and bind address.

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/grafana/alertmanager-webhook-proxy/pkg/proxy"
 	"github.com/grafana/alertmanager-webhook-proxy/pkg/server"
 	"github.com/grafana/alertmanager-webhook-proxy/pkg/templater"
 )
 
 func main() {
-	// TODO: Add configurable target host URL
-	tmpl, _ := templater.New("/tmp/awp/template.txt")
-	p, pErr := proxy.New("http://localhost:8090", tmpl)
-	srv := server.New(":8080", p)
+	tmplPtr := flag.String("template", "/tmp/awp/template.txt", "Path to alert payload transformation template")
+	targetPtr := flag.String("target", "http://localhost:8090", "Target URL")
+	addressPtr := flag.String("address", ":8080", "Server bind address")
+
+	flag.Parse()
+
+	tmpl, _ := templater.New(*tmplPtr)
+	p, pErr := proxy.New(*targetPtr, tmpl)
+	srv := server.New(*addressPtr, p)
 
 	if pErr != nil {
 		panic(pErr)
