b0f7e0e8ed9094e851429f0a8d52de6b48788597
Alex Martin <alex@suitupalex.com>
2022-04-28 20:34:33 -0500

    cmd: Update main entrypoint.

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"github.com/grafana/alertmanager-webhook-proxy/pkg/proxy"
 	"github.com/grafana/alertmanager-webhook-proxy/pkg/server"
+	"github.com/grafana/alertmanager-webhook-proxy/pkg/templater"
 )
 
 func main() {
 	// TODO: Add configurable target host URL
-	p, pErr := proxy.New("http://localhost:8090")
+	tmpl, _ := templater.New("/tmp/awp/template.txt")
+	p, pErr := proxy.New("http://localhost:8090", tmpl)
 	srv := server.New(":8080", p)
 
 	if pErr != nil {
